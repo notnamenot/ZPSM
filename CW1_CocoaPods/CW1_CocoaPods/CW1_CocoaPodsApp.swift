5adc35dc2a0160a7cadea3865b0c31a3c5e63c68
Izabela Pachel <33725887+notnamenot@users.noreply.github.com>
2021-10-12 12:57:40 +0200

    first

---
+++ CW1_CocoaPods/CW1_CocoaPods/CW1_CocoaPodsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CW1_CocoaPodsApp.swift
+//  CW1_CocoaPods
+//
+//  Created by Student on 12/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct CW1_CocoaPodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
