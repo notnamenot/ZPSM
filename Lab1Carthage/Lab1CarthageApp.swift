f6183b7fc026a927d5d8f680e92f1c1727fffcc9
Izabela Pachel <33725887+notnamenot@users.noreply.github.com>
2021-10-12 13:27:02 +0200

    porównanie 2_1

---
+++ Lab1Carthage/Lab1CarthageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1CarthageApp.swift
+//  Lab1Carthage
+//
+//  Created by Student on 12/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct Lab1CarthageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
