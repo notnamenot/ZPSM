f20765b4628637ba2ce9dab9e3ac83dd63f35442
Izabela Pachel <33725887+notnamenot@users.noreply.github.com>
2021-10-12 13:26:09 +0200

    porownianie3

---
+++ Lab1SwiftManager/Lab1SwiftManager/Lab1SwiftManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1SwiftManagerApp.swift
+//  Lab1SwiftManager
+//
+//  Created by Student on 12/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct Lab1SwiftManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
