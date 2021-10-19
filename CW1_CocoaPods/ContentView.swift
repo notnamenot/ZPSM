25a285786f5a3b7c680662444f224c69f66e3e87
Izabela Pachel <33725887+notnamenot@users.noreply.github.com>
2021-10-19 13:16:41 +0200

    json decoding

---
+++ CW1_CocoaPods/ContentView.swift
@@ -0,0 +1,60 @@
+//
+//  ContentView.swift
+//  CW1_CocoaPods
+//
+//  Created by Student on 12/10/2021.
+//
+
+import SwiftUI
+import Alamofire
+import SwiftyJSON
+//import JSONDecoder
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+                .padding()
+            Button("Title") {
+                AF.request("https://www.metaweather.com/api/location/44418/")
+                    //.response {
+                    //    response in debugPrint(response)
+                    //}
+                    .responseJSON { response in
+                        //let cond = response["consolidated_weather"]
+                        print(response)
+                        //print(response.request as Any)
+
+                        //let json = try? JSON(response.result.value!)
+                        
+                        switch response.result
+                        {
+                            case .success(let result):
+                                guard let json = try? JSON(data: result) else {print("x")  }
+                                /*let id = json["_id"].stringValue
+                                let color = json["avatarColor"].stringValue
+                                let avatarName = json["avatarName"].stringValue
+                                let email = json["email"].stringValue
+                                let name = json["name"].stringValue*/
+                            
+                                debugPrint(response.result as Any)
+                                //UserDataService.instance.setUserData(id: id, color: color, avatarName: avatarName, email: email, name: name)
+                                //completion(true)
+
+                            case .failure(let error):
+                                 //completion(false)
+                                 debugPrint(response.result as Any)
+                        }
+                    }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+ 
