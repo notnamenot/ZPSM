0697ae9528bd8935144023833cf48ed3fc0eb1b7
Izabela Pachel <33725887+notnamenot@users.noreply.github.com>
2021-11-16 13:24:57 +0100

    passat interfaceinterface

--- ML/MLSciKit/MLSciKit/ContentView.swift
+++ ML/MLSciKit/MLSciKit/ContentView.swift
@@ -9,34 +9,84 @@ import SwiftUI
 
 struct ContentView: View {
     
-    var przebieg = 0.0;
-    var rok = 1999;
-    var silnik = 1.2;
-    var paliwo = "diesel";
-    var cena = 0.0;
+    var cena: Double = 0.0;
+    
+//    let model = RandomForestPassatPriceUsd()
+    
+    var paliwa = ["gasoline", "diesel", "gas", "hybrid-petrol"]
+    
+//    @State private var selectedPaliwo = "gas"
+    
+    @State private var przebieg = 1000.0 //odometer_value
+    @State private var rok = 1950.0 // year_produced
+    
+    @State private var paliwo = "diesel"// engine_fuel
+    
+    @State private var silnik = 1.0 // engine_capacity
+    
+    @State private var isEditing = false
+    
+
+    func getPrice() {
+//        let defaultConfig = MLModelConfiguration()
+//        guard let model = try? RandomForestPassatPriceUsd(
+//        configuration: defaultConfig) else {
+//        fatalError("Could not create model.") }
+    }
+    
     
     var body: some View {
         VStack{
+            
             HStack{
                 Text("Przebieg: ")
                 Spacer()
-                //Slider()
+                Slider(
+                    value: $przebieg,
+                    in: 0...1000000,
+                    step: 500,
+                    onEditingChanged: { editing in isEditing=editing}
+                )
+                Text(String(format: " %.0f", przebieg))
             }.padding()
+            
             HStack{
                 Text("Rok produkcji: ")
                 Spacer()
-                //Slider()
+                Slider(
+                    value: $rok,
+                    in: 1980...2020,
+                    step: 1,
+                    onEditingChanged: { editing in isEditing=editing}
+                )
+                Text(String(format: " %.0f", rok))
             }.padding()
+
+            
             HStack{
-                Text("Pojemność silnika: ")
+                Text("Poj. silnika: ")
                 Spacer()
-                //Slider()
+                Slider(
+                    value: $silnik,
+                    in: 0.5...4,
+                    step: 0.01,
+                    onEditingChanged: { editing in isEditing=editing}
+                )
+                Text(String(format: " %.2f", silnik))
             }.padding()
+            
+            
             HStack{
                 Text("Rodzaj paliwa: ")
                 Spacer()
-                //Picker()
+                Picker("Choose paliwo", selection: $paliwo) {
+                    ForEach(paliwa, id: \.self) {
+                        p in Text(p.capitalized).tag(p)
+                    }
+                }.pickerStyle(.segmented)
             }.padding()
+            
+
             HStack{
                 Text("Cena:")
                 Text( String(format: " %.2f", "\(cena)"))
